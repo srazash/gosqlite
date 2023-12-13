00e2c646993a1b498ef4821ee1a2ce65f12703cc
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-12-13 11:59:10 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func main() {
+	fmt.Printf("I'm alive!\n")
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to open db")
+	}
+
+	// migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// create
+	db.Create(&Product{Code: "D69", Price: 420})
+
+	// read
+	var product Product
+	db.First(&product, 1)
+	db.First(&product, "code = ?", "D69")
+
+	// update
+	db.Model(&product).Update("Price", 840)
+	db.Model(&product).Updates(Product{Price: 210, Code: "Z96"})
+	db.Model(&product).Updates(map[string]interface{}{"Price": 420, "Code": "D69"})
+
+	// delete
+	//db.Delete(&product, 1)
+}
